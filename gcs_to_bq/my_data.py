310995bc543e1b9631709298e004febf6eca0b08
Prateek Kumar Saroj <95738391+prateekkumarsaroj@users.noreply.github.com>
2021-12-15 10:25:16 +0530

    Delete my_data.py

--- gcs_to_bq/my_data.py
+++
@@ -1,9 +0,0 @@
-import json
-with open('config.txt') as f:
-    data = f.read()
-    #print(data)
-    y = json.loads(data)
-    table =   y["table_name"]
-    mail_addr=y["recipient_email"]
-    mail_addr = y["recipient_email"]
-    #print(table)
